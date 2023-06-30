abbdf4b7ff98eddaef92e694c2bcc31b9d0d31a9
ManishTanganiya <manishtanganiya@gmail.com>
2023-06-30 18:07:20 +0530

    Initial commit

---
+++ SpringIOC/src/main/java/org/tnsif/springioc/DebitCard.java
@@ -0,0 +1,9 @@
+package org.tnsif.springioc;
+
+
+
+public interface DebitCard {
+	//abstract by default
+	void deposit();
+	void withdraw();
+}
---
+++ SpringIOC/src/main/java/org/tnsif/springioc/HDFCCard.java
@@ -0,0 +1,18 @@
+package org.tnsif.springioc;
+
+
+
+//Implementing a DebitCard interface
+public class HDFCCard implements DebitCard{
+
+	@Override
+	public void deposit() {
+		System.out.println("HDFC-Depositing an amount of 1 lakh rupees");
+	}
+
+	@Override
+	public void withdraw() {
+		System.out.println("HDFC-Depositing an amount of 1 lakh rupees");
+	}
+
+}
